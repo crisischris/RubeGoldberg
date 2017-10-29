ca47f0046966880c4d87330a8b082f1ef468233c
chris nelson <christopherthomasnelson@gmail.com>
2017-10-29 08:57:49 -0700

    Initial commit

---
+++ Assets/scripts/BallPhysics.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour {
+
+    public handInteraction handinteractionL;
+    public handInteraction handinteractionR;
+
+    public GameObject controllerL;
+    public GameObject controllerR;
+
+    private float bounceForce = 2f;
+    private LayerMask Interactive;
+
+    public float initXPos;
+    public float initYPos;
+    public float initZPos;
+
+    public float xpos;
+    public float ypos;
+    public float zpos;
+
+    public bool inPlayArea = true;
+
+    public Material mat1;
+    public Material mat2;
+
+
+
+
+    // Use this for initialization
+    void Start() {
+
+        initXPos = gameObject.transform.position.x;
+        initYPos = gameObject.transform.position.y;
+        initZPos = gameObject.transform.position.z;
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+        //check Left controller grabbing status
+        if (handinteractionL.isGrabbing == true)
+          { 
+            if(controllerL.transform.position.x < 1 && controllerL.transform.position.x > -1
+            && controllerL.transform.position.z < 1 && controllerL.transform.position.z > -1)
+            {
+                Debug.Log("you are out of bounds and grabbing something");
+                GetComponent<Renderer>().material = mat2;
+                GetComponent<Rigidbody>().mass = 1;
+                GetComponent<Rigidbody>().drag = 0;
+            }                         
+        }
+
+        if (handinteractionL.isGrabbing == true)
+        {
+            if(controllerL.transform.position.x > 1 || controllerL.transform.position.x < -1
+             || controllerL.transform.position.z > 1 || controllerL.transform.position.z < -1)
+            {
+                Debug.Log("you are  out of bounds and grabbing something");
+                GetComponent<Renderer>().material = mat1;
+                GetComponent<Rigidbody>().mass = 1000;
+                GetComponent<Rigidbody>().drag = 1000;
+            }             
+        }
+
+        //check Left controller grabbing status
+        if (handinteractionR.isGrabbing == true)
+        {
+            if (controllerR.transform.position.x < 1 && controllerR.transform.position.x > -1
+            && controllerR.transform.position.z < 1 && controllerR.transform.position.z > -1)
+            {
+                Debug.Log("you are out of bounds and grabbing something");
+                GetComponent<Renderer>().material = mat2;
+                GetComponent<Rigidbody>().mass = 1;
+                GetComponent<Rigidbody>().drag = 0;
+            }
+        }
+
+        if (handinteractionR.isGrabbing == true)
+        {
+            if (controllerR.transform.position.x > 1 || controllerR.transform.position.x < -1
+             || controllerR.transform.position.z > 1 || controllerR.transform.position.z < -1)
+            {
+                Debug.Log("you are  out of bounds and grabbing something");
+                GetComponent<Renderer>().material = mat1;
+                GetComponent<Rigidbody>().mass = 1000;
+                GetComponent<Rigidbody>().drag = 1000;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //reset ball position
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("ball hit the ground");         
+            gameObject.transform.position = new Vector3(initXPos, initYPos, initZPos);
+            Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+            rigidBody.velocity = rigidBody.velocity * 0;
+            rigidBody.angularVelocity = rigidBody.angularVelocity*0;
+            GetComponent<Renderer>().material = mat2;
+            GetComponent<Rigidbody>().mass = 1;
+            GetComponent<Rigidbody>().drag = 0;
+
+        }
+
+        //bounce ball 
+        if (col.gameObject.CompareTag("Bouncer"))
+        {
+            col.transform.SetParent(null);
+            Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+            rigidBody.velocity = rigidBody.velocity * bounceForce;
+            rigidBody.angularVelocity = rigidBody.angularVelocity;
+       //     Debug.Log("you have released the trigger");
+
+        }
+    }
+
+}
---
+++ Assets/scripts/handInteraction.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handInteraction : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+    public float throwForce = 1.5f;
+    public ushort pulesStregnth =  3000;
+
+    public GameObject controllerModel;
+
+    public float posXatThrow;
+    public float posYatThrow;
+    public float posZatThrow;
+
+    public bool isGrabbing = false;
+
+    // Use this for initialization
+    void Start () {
+
+        trackedObj = GetComponent<SteamVR_TrackedObject>();   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+    }    
+
+    void OnTriggerStay(Collider col)
+    {
+       if (col.gameObject.CompareTag("Throwable"))
+        {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                
+            
+                ThrowObject(col);
+                controllerModel.SetActive(true);
+            }
+            else if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(col);              
+                controllerModel.SetActive(false);
+            }           
+        }
+        if (col.gameObject.CompareTag("Interactive"))
+        {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                dropObject(col);
+                controllerModel.SetActive(true);
+            }
+            else if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(col);
+                controllerModel.SetActive(false);
+            }
+
+        }
+    }
+
+    
+
+   
+
+     void GrabObject(Collider col)
+    {
+       
+        col.transform.SetParent(gameObject.transform);
+        col.GetComponent<Rigidbody>().isKinematic = true;
+        device.TriggerHapticPulse(pulesStregnth);
+        isGrabbing = true;
+
+
+        // Debug.Log("you are touching down the trigger on an object");
+
+    }
+
+    void ThrowObject(Collider col)
+    {       
+
+        col.transform.SetParent(null);
+        Rigidbody rigidBody = col.GetComponent<Rigidbody>();
+        rigidBody.isKinematic = false;
+        rigidBody.velocity = device.velocity * throwForce;
+        rigidBody.angularVelocity = device.angularVelocity;
+        isGrabbing = false;
+
+
+
+
+        //  Debug.Log("you have released the trigger");
+
+
+    }  
+
+    void dropObject(Collider col)
+    {
+        col.transform.SetParent(null);
+        Rigidbody rigidBody = col.GetComponent<Rigidbody>();
+        rigidBody.isKinematic = false;
+        rigidBody.velocity = device.velocity * .9f;
+        rigidBody.angularVelocity = device.angularVelocity;
+        isGrabbing = false;
+        // Debug.Log("you have dropped the object");
+
+    }
+}

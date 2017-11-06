05202d0e0e4b2380976f213a5586f7809ca065b4
chris nelson <christopherthomasnelson@gmail.com>
2017-11-05 20:57:47 -0800

    Final w/ build
    final

--- Assets/scripts/Goal.cs
+++ Assets/scripts/Goal.cs
@@ -3,69 +3,61 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class Goal : MonoBehaviour {
-
+public class Goal : MonoBehaviour
+{
     public AudioSource[] sources;
 
-	// Use this for initialization
-	void Start () {
-
+    // Use this for initialization
+    void Start()
+    {
+        
         sources[0].Play();
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        int sceneNow = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("the current scene is" + sceneNow);
+    }
     void OnCollisionEnter(Collision col)
     {
-        Scene activeScene = SceneManager.GetActiveScene();
+        int activeScene = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("you are in scene index" + activeScene);
 
         if (col.gameObject.CompareTag("Throwable"))
         {
-            if(activeScene.name == "scene0")
-            sources[0].Stop();
-            sources[1].Play();            
-            SteamVR_LoadLevel.Begin("scene1", false, 2);
-        }
-
-        if (col.gameObject.CompareTag("Throwable"))
-        {
-            if (activeScene.name == "scene1")
+            if (activeScene == 0)
+            {
                 sources[0].Stop();
-            sources[1].Play();
-            SteamVR_LoadLevel.Begin("scene2", false, 2);
-        }
-
-        if (col.gameObject.CompareTag("Throwable"))
-        {
-            if (activeScene.name == "scene2")
+                sources[1].Play();
+                SteamVR_LoadLevel.Begin("scene0", false, 2);
+            }
+            if (activeScene == 1)
+            {
                 sources[0].Stop();
-            sources[1].Play();
-            SteamVR_LoadLevel.Begin("scene3", false, 2);
-        }
-
-        if (col.gameObject.CompareTag("Throwable"))
-        {
-            if (activeScene.name == "scene3")
+                sources[1].Play();
+                SteamVR_LoadLevel.Begin("scene1", false, 2);
+            }
+            if (activeScene == 2)
+            {
                 sources[0].Stop();
-            sources[1].Play();
-            SteamVR_LoadLevel.Begin("scene0", false, 2);
-        }
-
-        if (col.gameObject.CompareTag("Throwable"))
-        {
-            if (activeScene.name == "Intro")
+                sources[1].Play();
+                SteamVR_LoadLevel.Begin("scene2", false, 2);
+            }
+            if (activeScene == 3)
+            {
                 sources[0].Stop();
-            sources[1].Play();
-            SteamVR_LoadLevel.Begin("scene0", false, 2);
+                sources[1].Play();
+                SteamVR_LoadLevel.Begin("scene3", false, 2);
+            }
+            if (activeScene == 4)
+            {
+                sources[0].Stop();
+                sources[1].Play();
+                SteamVR_LoadLevel.Begin("Intro", false, 2);
+            }
         }
-
     }
-    
-          
 }
+
